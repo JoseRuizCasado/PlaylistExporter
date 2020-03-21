ecb5b6c9831d2d5ae4604f6390c386564081e6ad
JoseRuizCasado02 <joseblackstarx@gmail.com>
2020-03-21 18:07:26 +0100

    Project creation

---
+++ create_playlist.py
@@ -0,0 +1,14 @@
+
+class CreatePlaylist:
+
+    def __init__(self):
+        pass
+
+    def get_youtube_client(self):
+        pass
+
+    def get_liked_playlist(self):
+        pass
+
+    def create_playlist(self):
+        pass
