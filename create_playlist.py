022e36007ad58ef6921c8d5b61de4c8fbcd813fa
JoseRuizCasado02 <joseblackstarx@gmail.com>
2020-03-23 18:52:03 +0100

    create_playlist method almost implemented

--- create_playlist.py
+++ create_playlist.py
@@ -1,14 +1,51 @@
+import json
+import parameters
+import requests
 
 class CreatePlaylist:
 
-    def __init__(self):
-        pass
+    def __init__(self, playlist_name, public):
+        self.user_id = parameters.spotify_user_id
+        self.token = parameters.spotify_token
+        self.playlist_name = playlist_name
+        self.public = public
 
+    # Log into YouTube
     def get_youtube_client(self):
         pass
 
-    def get_liked_playlist(self):
+    # Grab the playlist to export into Spotify
+    def get__playlist_to_export(self):
         pass
 
+    # Create the playlist in Spotify
     def create_playlist(self):
+
+        request_body = json.dumps({
+            'name': self.playlist_name,
+            'description': 'Playlist exported from YouTube playlist',
+            'public': self.public
+        })
+
+        query = 'https://api.spotify.com/v1/users/{}/playlists'.format(self.user_id)
+
+        response = requests.post(
+            query,
+            data=request_body,
+            headers={
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer {}'.format(self.token)
+            }
+        )
+        response_json = response.json()
+
+        # Return the playlist id
+        return response_json[id]
+
+    # Search the song in Spotify
+    def get_spotify_uri(self):
+        pass
+
+    # Add the song into the new Spotify playlist
+    def add_song_to_playlist(self):
         pass
---
+++ parameters.py
@@ -0,0 +1,6 @@
+spotify_client_id = '260a89b63de248ec95e75cf634ac85bd'
+spotify_client_secret = '8555d845ccf546eaa6745f3625402729'
+
+spotify_user_id = ''
+
+spotify_token = ''
