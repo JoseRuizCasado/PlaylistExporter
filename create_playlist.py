66fd5155d3e73ee244300a334593f8ec9cde009f
JoseRuizCasado02 <joseblackstarx@gmail.com>
2020-03-25 12:09:47 +0100

    create_playlist and get_youtube_client methods fully implemented.

--- create_playlist.py
+++ create_playlist.py
@@ -1,6 +1,11 @@
 import json
 import parameters
 import requests
+import os
+
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
 
 class CreatePlaylist:
 
@@ -9,10 +14,30 @@ class CreatePlaylist:
         self.token = parameters.spotify_token
         self.playlist_name = playlist_name
         self.public = public
+        self.youtube_client = self.get_youtube_client()
 
     # Log into YouTube
     def get_youtube_client(self):
-        pass
+        # Copied from YouTube API
+        # Disable OAuthlib's HTTPS verification when running locally.
+        # *DO NOT* leave this option enabled in production.
+        os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+        api_service_name = 'youtube'
+        api_version = 'v3'
+        client_secrets_file = 'client_API_credential.json'
+
+        # Get credentials and create an API client
+        scopes = ['https://www.googleapis.com/auth/youtube.readonly']
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+            client_secrets_file, scopes)
+        credentials = flow.run_console()
+
+        # from the Youtube DATA API
+        youtube_client = googleapiclient.discovery.build(
+            api_service_name, api_version, credentials=credentials)
+
+        return youtube_client
 
     # Grab the playlist to export into Spotify
     def get__playlist_to_export(self):
@@ -40,11 +65,27 @@ class CreatePlaylist:
         response_json = response.json()
 
         # Return the playlist id
-        return response_json[id]
+        return response_json['id']
 
     # Search the song in Spotify
-    def get_spotify_uri(self):
-        pass
+    def get_spotify_uri(self, song_name, artist):
+        query = 'https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20'.format(
+            song_name,
+            artist
+        )
+        response = requests.post(
+            query,
+            data=request_body,
+            headers={
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer {}'.format(self.token)
+            }
+        )
+        response_json = response.json()
+        songs = response_json['tracks']['item']
+        # Get the first song uri
+        song_uri = songs[0]['uri']
+        return song_uri
 
     # Add the song into the new Spotify playlist
     def add_song_to_playlist(self):
